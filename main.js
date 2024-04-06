cb713de78724cfeae07a1d445a157a229cb7ca72
AnkitXGandhi <agproxy21@yahoo.com>
2024-04-06 20:02:54 +0530

    update 1.2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active')
+    navbar.classList.toggle('active');
 }
 
 // scroll section active link
@@ -11,15 +11,16 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll=()=> {
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height= sec.offsetHeight;
+        let height= sec.offsetHeight; 
         let id = sec.getAttribute('id');
+        
         if(top >= offset && top < offset + height) {
-            navLinks.forEach.apply(links => {
-                links.classList.remove('active');
+            navLinks.forEach(link => {
+                link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
@@ -27,7 +28,7 @@ window.onscroll=()=> {
     // sticky navbar
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
-    // remove toggle navbar
+    // remove toggle icon and navbar
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active'); 
 };
@@ -44,8 +45,8 @@ ScrollReveal().reveal('.home-contact p, .about-content', {origin:'right'});
 // typed js
 const typed = new Typed('.multiple-text',{
     strings: [' a Frontend Developer',' a Web Designer',' a UI/UX Designer',' Ankit Gandhi'],
-    typeSpeed:10,
-    backSpeed:10,
-    
+    typeSpeed:50,
+    backSpeed:20,
+    delay: 100,
     loop: false,
 });
